1eaaee8f5477a19d3ef5a6d497ed3e7f62f358e0
annmckh <y24.annmckh.com@gmail.com>
2026-09-19 13:23:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,5 @@
-const CACHE = "kakei-fixed-v1";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
-
-self.addEventListener("install", function(event){
-  event.waitUntil(
-    caches.open(CACHE).then(function(cache){ return cache.addAll(ASSETS); })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", function(event){
-  event.waitUntil(
-    caches.keys().then(function(keys){
-      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", function(event){
-  if(event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request).then(function(response){
-      var copy = response.clone();
-      caches.open(CACHE).then(function(cache){ cache.put(event.request, copy); });
-      return response;
-    }).catch(function(){
-      return caches.match(event.request).then(function(hit){
-        return hit || caches.match("./index.html");
-      });
-    })
-  );
-});
+const CACHE="kakei-monthly-v2";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(hit=>hit||caches.match("./index.html"))))});
